b60f00bd3d4499a83734342af598823f0d203728
EthernalSolitude <163121500+EthernalSolitude@users.noreply.github.com>
2026-02-06 13:15:25 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
+import os
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles  
 from app.api.routes import router
 
+
+RESULTS_DIR = "static/results"
+os.makedirs(RESULTS_DIR, exist_ok=True)
+
 app = FastAPI(title="OCR Service", description="Сервис для распознавания документов")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(router, prefix="/api")
 
